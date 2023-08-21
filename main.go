a82301875e52f5c42fcc46febbd928aca34db96e
Milan Santosi <milan.santosi@gmail.com>
2023-08-21 13:47:25 +0200

    added note to self to fix an artificial bug during the demo

--- main.go
+++ main.go
@@ -94,6 +94,7 @@ func generateHTMLTable(users []User, filename string) error {
 }
 
 func fetchUsersFromAPI() ([]User, error) {
+	// TODO: remove -1 during the demo to get all users!
 	userCount := getUserCount() - 1
 
 	url := apiBaseURL + "/users?per_page=" + strconv.Itoa(userCount)
